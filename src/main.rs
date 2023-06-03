e97e246cb101aef95b06a847f04bebb21feaf293
dangoldbj <dangoldbj23@gmail.com>
2023-06-03 11:27:16 +0200

    basic tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let listner = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (socket, _addr) = listner.accept().await.unwrap();
 }
