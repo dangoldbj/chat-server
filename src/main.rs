f7afdc91c181e9e195605dd99b76dafc44ecf863
dangoldbj <dangoldbj23@gmail.com>
2023-06-03 11:32:01 +0200

    simple echo server

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-use tokio::net::TcpListener;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
 
 #[tokio::main]
 async fn main() {
     let listner = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (socket, _addr) = listner.accept().await.unwrap();
+    let (mut socket, _addr) = listner.accept().await.unwrap();
+    loop {
+        let mut buffer = [0u8; 1024];
+
+        let bytes_read = socket.read(&mut buffer).await.unwrap();
+
+        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+    }
 }
